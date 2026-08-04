f7e5aeb872e3ba564063339f9bafae667ea14f52
A-lexit <105723402+A-lexit@users.noreply.github.com>
2026-08-04 07:00:29 +0300

    Update index5.php

--- api/index.php
+++ api/index.php
@@ -1,18 +1,17 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
+define('LARAVEL_START', microtime(true));
+
+// Перевірка на режим обслуговування (Maintenance mode)
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Завантаження автолоадера Composer
 require __DIR__.'/../vendor/autoload.php';
 
+// Запуск додатку Laravel (новий синтаксис для Laravel 11+)
 $app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
+$app->handleRequest(Request::capture());
