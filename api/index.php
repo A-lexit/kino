ba1f650cc17d04513cbcca2818d6ed4334383cca
A-lexit <105723402+A-lexit@users.noreply.github.com>
2026-08-04 16:29:06 +0300

    Update index21.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,14 @@
 <?php
-
 declare(strict_types=1);
 
-require __DIR__ . '/../public/index.php';
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    header('Content-Type: text/plain');
+    echo "ERROR: " . $e->getMessage() . "\n";
+    echo "FILE: " . $e->getFile() . ":" . $e->getLine() . "\n";
+    echo $e->getTraceAsString();
+}
