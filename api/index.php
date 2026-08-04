71acb57f0a7a5a406b1ab6f6dae01ac98b5255fd
A-lexit <105723402+A-lexit@users.noreply.github.com>
2026-08-04 15:35:12 +0300

    Update index16.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,17 @@
 <?php
 
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+set_exception_handler(function ($e) {
+    echo "<pre>";
+    echo $e;
+    echo "</pre>";
+});
+
+set_error_handler(function ($severity, $message, $file, $line) {
+    throw new ErrorException($message, 0, $severity, $file, $line);
+});
 
 require __DIR__.'/../public/index.php';
