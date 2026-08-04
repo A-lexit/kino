1f114a392f6734e9da90b30cd6cb5ae8fd6739e7
A-lexit <105723402+A-lexit@users.noreply.github.com>
2026-08-04 15:26:30 +0300

    Update index13.php

--- api/index.php
+++ api/index.php
@@ -1,15 +1,33 @@
 <?php
 
-use Illuminate\Http\Request;
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-define('LARAVEL_START', microtime(true));
+try {
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+    use Illuminate\Http\Request;
+
+    define('LARAVEL_START', microtime(true));
+
+    if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+        require $maintenance;
+    }
+
+    require __DIR__.'/../vendor/autoload.php';
+
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+
+    $app->handleRequest(Request::capture());
+
+} catch (Throwable $e) {
+
+    echo "<pre>";
+
+    echo $e->getMessage() . PHP_EOL . PHP_EOL;
+
+    echo $e->getFile() . ":" . $e->getLine() . PHP_EOL . PHP_EOL;
+
+    echo $e->getTraceAsString();
+
+    echo "</pre>";
 }
-
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
