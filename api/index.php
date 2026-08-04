a04f88b110f2670d3be63655b7100288cb3a0d36
A-lexit <105723402+A-lexit@users.noreply.github.com>
2026-08-05 02:29:21 +0300

    Update index62.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-use Illuminate\Http\Request;
+header('Content-Type: text/plain');
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -22,4 +22,24 @@ $_ENV['VIEW_COMPILED_PATH'] = $tmpStorage . '/framework/views';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 $app->useStoragePath($tmpStorage);
 
-$app->handleRequest(Request::capture());
+$app->singleton(\Illuminate\Contracts\Debug\ExceptionHandler::class, function ($app) {
+    return new class($app) extends \Illuminate\Foundation\Exceptions\Handler {
+        public function render($request, \Throwable $e)
+        {
+            return response(
+                "CAUGHT: " . get_class($e) . "\n" .
+                "MESSAGE: " . $e->getMessage() . "\n" .
+                "FILE: " . $e->getFile() . ":" . $e->getLine() . "\n\n" .
+                "TRACE:\n" . $e->getTraceAsString(),
+                500,
+                ['Content-Type' => 'text/plain']
+            );
+        }
+    };
+});
+
+$kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
+$request = \Illuminate\Http\Request::capture();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
