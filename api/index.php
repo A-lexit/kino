91ab22876775c17d2177210597ca4cea817cfb53
A-lexit <105723402+A-lexit@users.noreply.github.com>
2026-08-04 16:21:37 +0300

    Update index20.php

--- api/index.php
+++ api/index.php
@@ -1,36 +1,5 @@
 <?php
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+declare(strict_types=1);
 
-require __DIR__.'/../vendor/autoload.php';
-
-echo "autoload OK<br>";
-
-$app = require __DIR__.'/../bootstrap/app.php';
-
-echo "bootstrap OK<br>";
-
-var_dump(get_class($app));
-
-echo "<br>";
-
-echo "Has config? ";
-var_dump($app->bound('config'));
-
-echo "<br>";
-
-echo "Has events? ";
-var_dump($app->bound('events'));
-
-echo "<br>";
-
-echo "Has files? ";
-var_dump($app->bound('files'));
-
-echo "<br>";
-
-echo "Has log? ";
-var_dump($app->bound('log'));
-
-exit;
+require __DIR__ . '/../public/index.php';
