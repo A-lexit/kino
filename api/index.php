e60d570a8ed4acc055fd9ee0efdb72a1d9417242
A-lexit <105723402+A-lexit@users.noreply.github.com>
2026-08-05 17:44:39 +0300

    Update index97.php

--- api/index.php
+++ api/index.php
@@ -1,46 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$tmpStorage = '/tmp/storage';
-$tmpBootstrapCache = '/tmp/bootstrap/cache';
-
-$dirs = [
-    $tmpStorage . '/logs',
-    $tmpStorage . '/framework/cache/data',
-    $tmpStorage . '/framework/sessions',
-    $tmpStorage . '/framework/views',
-    $tmpStorage . '/app',
-    $tmpBootstrapCache,
-];
-foreach ($dirs as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0777, true);
-    }
-}
-
-$_ENV['VIEW_COMPILED_PATH'] = $tmpStorage . '/framework/views';
-$_ENV['APP_PACKAGES_CACHE'] = $tmpBootstrapCache . '/packages.php';
-$_ENV['APP_SERVICES_CACHE'] = $tmpBootstrapCache . '/services.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-$app->useStoragePath($tmpStorage);
-
-
-
-$kernel = $app->make(\Illuminate\Contracts\Http\Kernel::class);
-if (($_GET['debug_routes'] ?? false)) {
-    $kernel->bootstrap();
-    header('Content-Type: text/plain');
-    foreach (app('router')->getRoutes() as $route) {
-        echo implode('|', $route->methods()) . ' ' . $route->uri() . "\n";
-    }
-    exit;
-}
-
 $_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 
-$request = \Illuminate\Http\Request::capture();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
