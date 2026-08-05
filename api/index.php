f86dc62754beb0b119d0e8eab03f6099f95ac1e2
A-lexit <105723402+A-lexit@users.noreply.github.com>
2026-08-05 17:56:49 +0300

    Update index99.php

--- api/index.php
+++ api/index.php
@@ -38,7 +38,8 @@ if (($_GET['debug_routes'] ?? false)) {
     exit;
 }
 
-
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 
 $request = \Illuminate\Http\Request::capture();
 $response = $kernel->handle($request);
