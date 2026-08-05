8648d78e6b7157944018847940560be3908d1ecc
A-lexit <105723402+A-lexit@users.noreply.github.com>
2026-08-05 03:23:21 +0300

    Update api66.php

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('film-json', [App\Http\Controllers\Api\FilmController::class, 'show']);
+Route::get('film-json', [App\Http\Controllers\Api\FilmController::class, 'show'])->name('film-json');
 
 
-Route::post('film-views-increment', [App\Http\Controllers\Api\FilmController::class, 'viewsIncrement']);
-Route::post('film-likes-increment', [App\Http\Controllers\Api\FilmController::class, 'likesIncrement']);
+Route::post('film-views-increment', [App\Http\Controllers\Api\FilmController::class, 'viewsIncrement'])->name('film-views-increment');
+Route::post('film-likes-increment', [App\Http\Controllers\Api\FilmController::class, 'likesIncrement'])->name('film-likes-increment');
 
-Route::middleware(['web', 'auth'])->post('film-add-comment', [App\Http\Controllers\Api\CommentController::class, 'store']);
+Route::middleware(['web', 'auth'])->post('film-add-comment', [App\Http\Controllers\Api\CommentController::class, 'store'])->name('film-add-comment');
 
 
 Route::fallback(function() {
