a03c6bf89760d5e9c39f71ef236391137956d27e
Олексій <megasiteseo@gmail.com>
2026-08-05 13:50:31 +0000

    Update bootstrap.js axios

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -14,6 +14,9 @@
     <meta property="og:description" content="@yield('description', $settings->description ?? '')">
     <meta property="og:url" content="{{ url()->current() }}">
 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
 
     <meta name="twitter:card" content="summary_large_image">
 
