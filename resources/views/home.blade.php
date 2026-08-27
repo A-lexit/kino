7a09a48673026e4ee325295784ba1f21d54193b8
A-lexit <105723402+A-lexit@users.noreply.github.com>
2026-08-28 02:03:55 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 
         <x-home-section :films="$films" title="Фільми" category-slug="filmi" />
         <x-home-section :films="$serials" title="Серіали" category-slug="seriali" />
-        <x-home-section :films="$mults" title="Мультфільми" category-slug="multiki" />
+        <x-home-section :films="$mults" title="Мультфільми" category-slug="multfilmi" />
         <x-home-section :films="$multserials" title="Мультеріали" category-slug="multseriali" />
 
     </div>
